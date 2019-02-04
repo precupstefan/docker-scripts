e35e67af3581cc25bd80195bf0fafaa915fde481
Stefan Precup <psa9710@gmail.com>
2019-02-04 11:21:09 +0200

    Add symbolic link to script.sh

--- nginx-php-mysql/nginx/script.sh
+++ nginx-php-mysql/nginx/script.sh
@@ -3,5 +3,6 @@ echo Intializing nginx
 
 echo "127.0.0.1 WEBSITE" >> /etc/hosts
 sed -i '$iinclude /etc/nginx/sites-enabled/*;' /etc/nginx/nginx.conf
+ln -s /websites/wtp /var/www/WEBSITE
 echo Initialization finished
 exec nginx -g 'daemon off;'
