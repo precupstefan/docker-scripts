4e004bda330fc0941d5564dab25fdb6b7b73791b
Stefan Precup <psa9710@gmail.com>
2019-02-04 11:16:25 +0200

    Add script execution on startup

---
+++ nginx-php-mysql/nginx/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Intializing nginx
+
+echo "127.0.0.1 WEBSITE" >> /etc/hosts
+sed -i '$iinclude /etc/nginx/sites-enabled/*;' /etc/nginx/nginx.conf
+echo Initialization finished
+exec nginx -g 'daemon off;'
